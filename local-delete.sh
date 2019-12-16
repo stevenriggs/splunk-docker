5f53b46f54990c279703baf3a5e16c76ccca7e22
Steven Riggs <steven.riggs@icloud.com>
2019-12-16 16:04:06 -0500

    add kubernetes local deployment

---
+++ local-delete.sh
@@ -0,0 +1,40 @@
+# Copyright (c) 2019, Steven Riggs (http://stevenriggs.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+##########################
+
+echo "ensure the correct environment is selected..."
+KUBECONTEXT=$(kubectl config view -o template --template='{{ index . "current-context" }}')
+if [ "$KUBECONTEXT" != "docker-desktop" ]; then
+	echo "ERROR: Script is running in the wrong Kubernetes Environment: $KUBECONTEXT"
+	exit 1
+else
+	echo "Verified Kubernetes context: $KUBECONTEXT"
+fi
+
+##########################
+
+echo "delete splunk...."
+kubectl delete -f ./kubernetes/splunk.yaml
+
+##########################
+
+echo "delete the persistent volume for splunk...."
+kubectl delete -f ./kubernetes/splunk-local-pv.yaml
+rm -rf /Users/Shared/Kubernetes/persistent-volumes/default/splunk
+
+##########################
+
+echo "...done"
